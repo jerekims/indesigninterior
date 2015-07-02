0e218caf65e1d7a99380fc41a3fc64a3e4a4d6dd
Willy Mareka <marekawilly@gmail.com>
2015-07-02 23:37:09 +0300

    started working on footer

--- application/modules/home/views/footer_view1.php
+++ application/modules/home/views/footer_view1.php
@@ -2,10 +2,13 @@
           <div class="container">
             <div class="row">
               <div class="col l6 s12">
-                <h5 class="white-text">Footer Content</h5>
-                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                <h3 class="white-text">Indesign Interior</h3>
+                <p class="grey-text text-lighten-4">imagine /design /create</p>
+                <hr/>
               </div>
+
               <div class="col l4 offset-l2 s12">
+               <div class="vertical-line"></div>
                 <h5 class="white-text">Links</h5>
                 <ul>
                   <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
@@ -14,12 +17,13 @@
                   <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
                 </ul>
               </div>
+
             </div>
           </div>
           <div class="footer-copyright">
             <div class="container">
-            © 2014 Copyright Text
-            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            © 2015 All Rights Reserved
+            <a class="grey-text text-lighten-4 right" href="#!">indesigninteriorltd@yahoo.com</a>
             </div>
           </div>
         </footer>
